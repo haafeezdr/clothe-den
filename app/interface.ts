8802c0f6e2a96e23f97e7fd0bae17233b86ac984
haafeezdr <haafeezdaiyab>
2023-12-09 22:22:21 +0100

    home page

---
+++ app/interface.ts
@@ -0,0 +1,8 @@
+export interface simplifiedProduct {
+    _id: string,
+    name: string,
+    price: number,
+    slug: string,
+    categoryName: string,
+    imageUrl: string
+}
---
+++ app/product/[slug]/page.tsx
@@ -0,0 +1,3 @@
+export default function ProductPage() {
+    return <div>hello from the Product Page</div>;
+}
